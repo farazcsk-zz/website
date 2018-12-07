5d08d0281be91fa9b0746d0f20b36431e1dac1d8
Faraz Khan <farazcsk@gmail.com>
2018-12-07 08:56:06 +0000

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
